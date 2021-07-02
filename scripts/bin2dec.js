ce9b2b48db57df98566b92573fc46ee0ba0dab30
JohnAnon9771 <njoao97710@gmail.com>
2021-07-02 14:59:28 -0300

    :tada: finish project

--- scripts/bin2dec.js
+++ scripts/bin2dec.js
@@ -1,1 +1,10 @@
-export default function bin2dec() {}
+function bin2dec(binary) {
+  let binaryToString = binary.toString()
+  let result = 0
+  for (let i = 0; i < binaryToString.length; i++) {
+    result = result * 2 + binaryToString.charCodeAt(i) - 48
+  }
+  return result
+}
+
+module.exports = bin2dec
--- scripts/bin2dec.test.js
+++ scripts/bin2dec.test.js
@@ -1,7 +1,11 @@
 import bin2dec from './bin2dec'
 
-describe('bin2dec function', () => {
+describe('bin2dec', () => {
   it('should exists', () => {
     expect(bin2dec).toBeDefined()
+  })
+
+  it('should convert a binary number to decimal', () => {
+    expect(bin2dec(101)).toBe(5)
   })
 })
